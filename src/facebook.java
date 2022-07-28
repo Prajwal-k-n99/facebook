60c817cb25a063e529a7b93e2660f01d2ec143e3
Prajwal K N <prajwalkpura99@gmail.com>
2022-07-28 23:41:47 +0530

    fb

--- src/facebook.java
+++ src/facebook.java
@@ -1,38 +1,58 @@
 import java.util.Scanner;
 
-class facebook {
-    String name ="Prajwal" ;
+class Profile {
+    String name;
     long id;
-    String comment;
-    String like;
-    String search(){
-        return name;
-    }
-    void post(){
-        System.out.println("Select images");
-        System.out.println("Post the images");
-    }
-    void like(){
-        System.out.println("Liking the post");
-    }
-    void comment(){
-        System.out.println("Enter the comment");
+    long password;
+
+    void login() {
         Scanner sc = new Scanner(System.in);
-        comment = sc.nextLine();
+        System.out.println("Enter your ID");
+        id = sc.nextLong();
+        System.out.println("Enter your password");
+        password = sc.nextLong();
+    }
+        String search(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the name");
+            name = sc.nextLine();
+            return name;
+        }
+    }
+    class Post{
+        String comment;
+        String like;
+        void post(){
+            System.out.println("Select images");
+            System.out.println("Post the images");
+    }
+    void like() {
+        for (int i = 0; i>=0; i++) {
+            System.out.println("Liking the post");
+        }
+    }
+    void comment() {
+        for (int i = 0; i >= 0; i++) {
+            System.out.println("Enter the comment");
+            Scanner sc = new Scanner(System.in);
+            comment = sc.nextLine();
+        }
     }
     void friendrequest(){
         System.out.println("Send a friend request");
         System.out.println("Accept request");
     }
 }
-class socialnetwork{
+class facebook{
     public static void main(String[] args) {
-        facebook myfb = new facebook();
-        myfb.search();
-        myfb.comment();
-        myfb.like();
-        myfb.post();
-        myfb.friendrequest();
+        Profile myprofile = new Profile();
+        Post mypost = new Post();
+        myprofile.login();
+        myprofile.search();
+        mypost.post();
+        mypost.comment();
+        mypost.like();
+        mypost.friendrequest();
     }
 }
 
